380a99086cd2ccbe0c3cfbcaea5cc78478bed05d
yiguihai <yiguihai@gmail.com>
2018-06-17 03:32:12 +0800

    修复成吨bug

--- Admin/mobile.php
+++ Admin/mobile.php
@@ -10,9 +10,20 @@
   <title>关于手机</title>
  </head>
  <body ontouchstart="">
-<div class="demo-item"><p class="demo-desc">系统信息</p><div class="demo-block">
- <ul class="ui-list ui-list-single ui-list-link ui-border-tb">       
-<?php
+ <section id="tab">
+    <div class="demo-item">
+        <p class="demo-desc">系统状态</p>
+        <div class="demo-block">
+            <div class="ui-tab">
+                <ul class="ui-tab-nav ui-border-b">
+                  <li class="current"><span>系统信息</span></li>
+                  <li><span>电池信息</span></li>
+                  <li><span>内存信息</span></li>
+                </ul>
+                <ul class="ui-tab-content" style="width:300%">
+                    <li>
+                    <?php 
+    echo "<ul class=\"ui-list ui-list-single ui-list-link ui-border-tb\">";
     echo "<li class=\"ui-border-t\"><div class=\"ui-list-info\"><h4 class=\"ui-nowrap\">型号</h4><div class=\"ui-txt-info\">".shell_exec('getprop ro.product.model')."</div></div></li>";
     echo "<li class=\"ui-border-t\"><div class=\"ui-list-info\"><h4 class=\"ui-nowrap\">正式名称</h4><div class=\"ui-txt-info\">".shell_exec('getprop ro.product.name')."</div></div></li>";
     echo "<li class=\"ui-border-t\"><div class=\"ui-list-info\"><h4 class=\"ui-nowrap\">Android版本</h4><div class=\"ui-txt-info\">".shell_exec('getprop ro.build.version.release')."</div></div></li>";
@@ -29,8 +40,11 @@
     $signal_text=strpos($signal, 'SignalStrength: ');
     $nub=explode(' ', substr($signal,$signal_text,100));
     echo "<li class=\"ui-border-t\"><div class=\"ui-list-info\"><h4 class=\"ui-nowrap\">SIM信号强度</h4><div class=\"ui-txt-info\">".$nub[9]."</div></div></li>";
-    echo "</div></div>";
-    echo "<div class=\"demo-item\"><p class=\"demo-desc\">电池信息</p><div class=\"demo-block\"><ul class=\"ui-list ui-list-single ui-list-link ui-border-tb\">";
+    echo "</ul>"; ?>
+                    </li>
+                    <li>
+                    <?php 
+    echo "<ul class=\"ui-list ui-list-single ui-list-link ui-border-tb\">";
     $battery_file=shell_exec('su -c cat /sys/class/power_supply/battery/uevent');
     $battery_arr = parse_ini_string($battery_file);
     foreach ($battery_arr as $key => $value) {
@@ -46,12 +60,55 @@
         if($key=="POWER_SUPPLY_TEMP") {
           $value="(电池温度 ".($value/10)." °C)";
         }
-        //print_r($key."=".$value."<br>");
         echo "<li class=\"ui-border-t\"><div class=\"ui-list-info\"><h4 class=\"ui-nowrap\">".$key."</h4><div class=\"ui-txt-info\">".$value."</div></div></li>";
     }
-?>
-</ul>
-</div>
-</div>
+    echo "</ul>"; ?>
+                    </li>
+                    <li>
+                    <?php 
+    echo "<ul class=\"ui-list ui-list-single ui-list-link ui-border-tb\">";
+    foreach (explode(PHP_EOL, file_get_contents('/proc/meminfo')) as $key) {
+    $key=explode(':', $key);
+    foreach (explode('kB', $key[1]) as $value) {
+    $value=trim($value);
+        if ($value > 0) { 
+        echo "<li class=\"ui-border-t\"><div class=\"ui-list-info\"><h4 class=\"ui-nowrap\">".$key[0]."</h4><div class=\"ui-txt-info\">".round($value/1024,2)." MB</div></div></li>";
+        }
+    }
+}
+    echo "</ul>"; ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="../js/zepto.min.js"></script>
+<script type="text/javascript">
+(function() {
+    var record = 0;
+    var origin_l;
+    $('.ui-tab-nav').eq(0).find('li').on('click',function() {
+                  //记录原始点的位置:
+                //   if(!record){
+                //       origin_l = $('.ui-tab-nav li.current i').offset().left;
+                //       record++;
+                //   }
+                  $(this).parent().find('li').removeClass('current');
+                  $(this).addClass('current');
+                //   var l = $(this).find('span').offset().left;
+                //   var w = $(this).find('span').offset().width;
+                //   $('.ui-tab-nav li.current i').css({
+                //       'transform':'translate3d('+(l-origin_l)+'px,0,0)',
+                //       'width':w,
+                //       'transition':'transform 0.5s linear'
+                //   });
+                  $('.ui-tab-content').eq(0).css({
+                    'transform':'translate3d(-'+($(this).index()*$('.ui-tab-content li').offset().width)+'px,0,0)',
+                    'transition':'transform 0.5s linear'
+                })
+    });
+})(window, undefined)
+</script>
 </body>
 </html>

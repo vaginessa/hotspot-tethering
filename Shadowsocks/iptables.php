b5100387efbefb2b842b2a7513934c7ff6beecd8
yiguihai <yiguihai@gmail.com>
2018-06-29 07:50:18 +0800

    提交信息

--- Shadowsocks/iptables.php
+++ Shadowsocks/iptables.php
@@ -22,9 +22,6 @@ $mangle = array(
     // 添加路由策略，让所有经 TPROXY 标记的 0x2333/0x2333 udp 数据包使用路由表 123
     "ip rule add fwmark 0x2333/0x2333 table 123",
     "iptables -t mangle -A redsocks2_out -j redsocks2_lan",
-    "iptables -t mangle -A redsocks2_out -o lo -j ACCEPT",
-    "iptables -t mangle -A redsocks2_out -o tun+ -j ACCEPT",
-    "iptables -t mangle -A redsocks2_out -s 192.168/16 -j ACCEPT",
     "iptables -t mangle -A redsocks2_out -m owner --uid-owner 3004 -j ACCEPT",
     "iptables -t mangle -A redsocks2_out -p udp -j MARK --set-mark 0x2333/0x2333",
     "iptables -t mangle -A PREROUTING -j redsocks2_pre",
@@ -38,10 +35,8 @@ $nat = array(
     "iptables -t nat -N out_forward",
     "iptables -t nat -N koolproxy_forward",
     //本机发出同意
-    "iptables -t nat -A out_lan -o lo -j ACCEPT",
-    "iptables -t nat -A out_lan -o tun+ -j ACCEPT",
+    "iptables -t nat -A out_lan -d 10/8 -j ACCEPT",
     "iptables -t nat -A out_lan -d 127/8 -j ACCEPT",
-    "iptables -t nat -A out_lan -s 192.168/16 -j ACCEPT",
     "iptables -t nat -A out_lan -d 192.168/16 -j ACCEPT",
     "iptables -t nat -A out_lan -m owner --uid-owner 3004 -j ACCEPT",
     //"iptables -t nat -A out_lan -p tcp -m owner ! --uid-owner $(id -u) -j koolproxy_forward",
@@ -143,7 +138,7 @@ function iptables_start($mangle, $nat, $filter, $stop_iptables, $status_binary, 
                 file_put_contents($tmp_file, $value . PHP_EOL, FILE_APPEND | LOCK_EX);
             }
             if ($server) {
-             file_put_contents($tmp_file, "iptables -t mangle -I redsocks2_out 3 -d $server -j ACCEPT".PHP_EOL, FILE_APPEND | LOCK_EX);
+             file_put_contents($tmp_file, "iptables -t mangle -A redsocks2_lan -d $server -j ACCEPT".PHP_EOL, FILE_APPEND | LOCK_EX);
             }
         }
     }
@@ -163,7 +158,7 @@ function iptables_start($mangle, $nat, $filter, $stop_iptables, $status_binary, 
         file_put_contents($tmp_file, "iptables -t nat -A out_lan -p udp ! --dport 53 -j DNAT --to-destination 127.0.0.1".PHP_EOL, FILE_APPEND | LOCK_EX); 
         }
         if ($server) {
-             file_put_contents($tmp_file, "iptables -t nat -I out_lan 5 -d $server -j ACCEPT".PHP_EOL, FILE_APPEND | LOCK_EX);
+             file_put_contents($tmp_file, "iptables -t nat -I out_lan 4 -d $server -j ACCEPT".PHP_EOL, FILE_APPEND | LOCK_EX);
         }
     }
     foreach ($filter as $value) {

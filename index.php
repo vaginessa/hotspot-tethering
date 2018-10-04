ac3e345bed67cf45dfff58dfebd4a49732477daa
yiguihai <18776473475@163.com>
2018-10-05 03:56:30 +0800

    更新index.php定向
    更智能的网页定向，修复以前出现的问题。

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
+<?php 
+function siteURL()
+{
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $domainName = $_SERVER['HTTP_HOST'].'/';
+    return $protocol.$domainName;
+}
 header("HTTP/1.1 302 Found");
-header("Location: Welcom/");
+header("Location: ".siteURL()."/Welcom");
 ?>
